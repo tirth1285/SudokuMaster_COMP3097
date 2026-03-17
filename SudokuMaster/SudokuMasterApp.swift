5ab636639c3a409f85cd0baa124a57c14df7de60
hetj2004 <het28jasani.com>
2026-03-17 15:57:54 -0400

    GameView updated

--- SudokuMaster/SudokuMasterApp.swift
+++ SudokuMaster/SudokuMasterApp.swift
@@ -2,16 +2,19 @@
 //  SudokuMasterApp.swift
 //  SudokuMaster
 //
-//  Created by TIRTH RABADIYA on 2026-02-06.
+//  Created by HET JASANI on 2026-02-06.
 //
-
 import SwiftUI
 
 @main
 struct SudokuMasterApp: App {
+    
+    @StateObject var scoreManager = ScoreManager()
+    
     var body: some Scene {
         WindowGroup {
             LaunchView()
+                .environmentObject(scoreManager)
         }
     }
 }
